58c745e984b1f05ef9955a6f5e25c82704f0a7fd
peng fu <fupenglxy@gmail.com>
2019-02-23 21:10:38 +0800

    first

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  title: 'home';
+
+  constructor(private router: Router) { }
+
+  toLogin() {
+    this.router.navigateByUrl('login');
+  }
+
+  ngOnInit() {
+    console.log('init');
+  }
+
+}
